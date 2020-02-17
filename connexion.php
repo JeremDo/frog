145beac7159022c5ca28471e345a5f25d9a8a0ce
JeremDo <38661963+JeremDo@users.noreply.github.com>
2020-02-17 09:34:46 +0100

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -5,8 +5,8 @@ class Connexion{
     public $bdd;
     public function __construct(){
         if(!Connexion::$ONLINE){
-            $this->bdd = new PDO('mysql:host=http://www.jdub.fr;dbname=Database operations
-db746191009', 'dbo746191009', 'Proxima01!', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+            $this->bdd = new PDO('mysql:host=localhost;dbname=Database operations
+db746191009', 'dbo746191009', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
         }
         else{
             //mettre ici les infos de connexion sur un service distant. 
